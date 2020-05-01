13fcd0ca3ddbefab3e0208ef6968c1787d5b1843
Powi <powi@powi.fr>
2020-05-01 11:59:22 +0200

    Translatable + translate in french

--- init.lua
+++ init.lua
@@ -1,47 +1,50 @@
 --By @Farfadet46
 
 local maximus=10
-local valeur_de = 0
+local value_of = 0
+
+-- Load support for MT game translation.
+local S = minetest.get_translator("dice")
 
 minetest.register_chatcommand("dice_max", {
 	func = function(name, param)
-		if param == "" or tonumber(param) > 10 then
+		if param == "" or tonumber(param) > maximus then
 			minetest.chat_send_player(name, "------------------------------------")
-			minetest.chat_send_player(name, " ** Parametres incorrects - Veuillez renseigner la valeur maximum du DE. Elle ne peut pas depasser 10. **")
+			minetest.chat_send_player(name, S("** Incorrect settings - Please set a maximum value for the DICE. It can't be upper than 10.**."))
 			minetest.chat_send_player(name, "------------------------------------")
-			minetest.chat_send_player(name, " ** Rappel de la valeur du DE : ".. valeur_de .." **")
+			minetest.chat_send_player(name, S("** Reminder, the value of the DICE is @1 **",param))
 			return
 		else
 		maximus = param
-		minetest.chat_send_player(name, "La valeur maximum du DE est maintenant de : " ..param )
+		minetest.chat_send_player(name, S("The maximum value of the DICE is now : @1",param))
 		end
 	end
 	
 })
 
-minetest.register_node("dice:blocos_1", {
-	description = "Dice_1",
+minetest.register_node("dice:blocos", {
+	description = S("Dice"),
 	drawtype = "nodebox",
 	paramtype = "light",
 	paramtype2 = "facedir",
-	tiles = {"dice_blocos_1.png"},
+	tiles = {"dice_blocos.png"},
 	drop = "",
 	inventory_image = "dice_blocos.png",
-	wield_image = "dice_blocos_1.png",
+	wield_image = "dice_blocos.png",
 	groups = {dig_immediate=3},
 	sounds = default.node_sound_wood_defaults(),
 	
 	on_place = function(itemstack, placer, pointed_thing)
-		valeur_de = math.random(1,maximus)
-		local stack = ItemStack("dice:blocos_".. valeur_de)
+		value_of = math.random(1,maximus)
+		local stack = ItemStack("dice:blocos_".. value_of)
 		local ret = minetest.item_place(stack, placer, pointed_thing)
-		minetest.chat_send_all( "Le DE a fait : " ..valeur_de )
+		minetest.chat_send_all( S("The DICE did : @1",value_of) )
 	end,
 })
 
-for i=2,maximus do
+for i=1,maximus do
 	minetest.register_node("dice:blocos_"..i, {
-		description = "Dice_"..i,
+		description = S("Dice_@1",i),
 		drawtype = "nodebox",
 		paramtype = "light",
 		paramtype2 = "facedir",
